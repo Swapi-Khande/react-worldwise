ea97d9adeecf1de10b718c3b9d7d22c97fb3851d
Swapi_Khande <swapnilkhande6996@gmail.com>
2024-05-08 20:04:57 +0530

    React router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
 
 export default function App() {
   return (
-    <div>Worldwise App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />}/>
+        <Route path="product" element={<Product />}/>
+        <Route path="pricing" element={<Pricing />}/>
+        <Route path="*" element={<PageNotFound />}/>
+      </Routes>
+    
+    </BrowserRouter>
   )
 }
