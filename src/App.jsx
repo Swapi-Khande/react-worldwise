62be3813f076e462fd5fc249cd8fd1e96e34c396
Swapi_Khande <swapnilkhande6996@gmail.com>
2024-05-10 16:06:42 +0530

    App layout added

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,19 @@ import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />}/>
-        <Route path="product" element={<Product />}/>
-        <Route path="pricing" element={<Pricing />}/>
-        <Route path="/login" element={<Login />}/>
-        <Route path="*" element={<PageNotFound />}/>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/app" element={<AppLayout />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
-    
     </BrowserRouter>
-  )
+  );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Logo from "./Logo";
+import AppNav from "./AppNav";
+import Styles from "./Sidebar.module.css";
+
+export default function Sidebar() {
+  return (
+    <div className={Styles.sidebar}>
+      <Logo />
+      <AppNav />
+
+      <p>List of cities</p>
+      <footer className={Styles.footer}>
+        <p className={Styles.copyright}>
+          &copy; Copyright {new Date().getFullYear()} by WorldWide Inc.
+        </p>
+      </footer>
+    </div>
+  );
+}
